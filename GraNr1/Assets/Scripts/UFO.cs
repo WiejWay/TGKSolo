60eaa754d9ec5974b9a1182b0e4a9cfd8ae5f6a7
w <w@wp.pl>
2023-10-12 13:50:16 +0200

    EndGame

--- GraNr1/Assets/Scripts/UFO.cs
+++ GraNr1/Assets/Scripts/UFO.cs
@@ -13,25 +13,25 @@ public class UFO : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector2.right * Time.deltaTime * 10f);
+            transform.Translate(Vector2.right * Time.deltaTime * 15f);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector2.up * Time.deltaTime * 10f);
+            transform.Translate(Vector2.up * Time.deltaTime * 15f);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
  
-            transform.Translate(Vector2.left * Time.deltaTime * 10f);
+            transform.Translate(Vector2.left * Time.deltaTime * 15f);
         }
 
 
         if (Input.GetKey(KeyCode.S))
         {
 
-            transform.Translate(Vector2.down * Time.deltaTime * 10f);
+            transform.Translate(Vector2.down * Time.deltaTime * 15f);
         }
     }
 }
